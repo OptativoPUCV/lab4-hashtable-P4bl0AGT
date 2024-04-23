91c610f40e049769f9ada646e22b31336926efc0
P4bl0AGT <160780317+P4bl0AGT@users.noreply.github.com>
2024-04-23 01:47:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,8 +53,7 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity)
 {
-  HashMap *map = (HashMap *) malloc(sizeof(HashMap) * capacity); 
-  return map;
+  return NULL;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
