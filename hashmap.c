9c633143c3526e2a9d7d38c48ba3a7a6a72fd1f0
P4bl0AGT <160780317+P4bl0AGT@users.noreply.github.com>
2024-04-23 03:44:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -102,15 +102,14 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map)
 {
-  long pos = 0;
-  Pair *pair = map->buckets[pos];
-  while(pair->key == NULL)
+  for(long i = 0 ; i < map->capacity ; i++)
   {
-    pos++;
-    pair = map->buckets[pos % map->capacity];
-    
+    Pair *pair = map->buckets[i];
+    if(pair != NULL)
+      return pair;
   }
-  return pair;
+  
+  return NULL;
   
 }
 
