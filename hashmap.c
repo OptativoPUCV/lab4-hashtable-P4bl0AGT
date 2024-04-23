b50f96c68b669b32bdd605d2ffcbacd50c170600
P4bl0AGT <160780317+P4bl0AGT@users.noreply.github.com>
2024-04-23 03:13:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,8 +39,11 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
-
+void insertMap(HashMap * map, char * key, void * value)
+{
+  Pair *pair = createPair(key, value);
+  long hash = hash(key, value);
+  map->buckets[hash] = pair;
 
 }
 
