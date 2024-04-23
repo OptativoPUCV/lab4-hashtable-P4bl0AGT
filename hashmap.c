3c988db1b91b9aca27096e17720d287f68780392
P4bl0AGT <160780317+P4bl0AGT@users.noreply.github.com>
2024-04-23 03:58:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -92,7 +92,7 @@ Pair * searchMap(HashMap * map,  char * key) {
   for(long i = pos ; i < map->capacity ; i++)
   {
     Pair *pair = map->buckets[i];
-    if( pair != NULL && strcmp(pair->key, key) == 0 )
+    if( pair != NULL && !strcmp(pair->key, key) )
     {
       map->current = i;
       return pair;
