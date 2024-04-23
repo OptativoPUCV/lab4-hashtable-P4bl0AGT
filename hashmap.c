f0318203652f430a5dc6c6cf7a0aece71ff7a0c4
P4bl0AGT <160780317+P4bl0AGT@users.noreply.github.com>
2024-04-23 03:05:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,7 +54,7 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity)
 {
   HashMap *map = (HashMap *) malloc(sizeof(HashMap) * capacity); 
-  map->buckets = NULL;
+  map->buckets = (Pair **) malloc(sizeof(Pair *) * capacity);
   for(long i = 0 ; i < capacity ; i++)
     map->buckets[i] = NULL;
   map->size = 0;
